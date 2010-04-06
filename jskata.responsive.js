babff309622bcc5619155c1e50bc3d9d69452736
Dan Simard <dsimard@gmail.com>
2010-04-06 18:01:35 -0400

    first working version

--- jskata.responsive.js
+++ jskata.responsive.js
@@ -1,16 +1,58 @@
 (function() {
-
   var jsk = {
-    for:function(wh, inc, fct) {
-      do {
-        fct();
-        inc();
-      } while (wh())      
+    sleepFor : 50, // How many milliseconds should it sleeps
+    chunkSize : 10, // How many executions before sleep
+    stopped : false,
+    // A responsive for
+    // Options accept "sleepFor" and "chunkSize"
+    for:function(wh, inc, fct, options) {
+      var that = this; // used as "this" in shouldContinue
+      
+      var sleepFor = options && options["sleepFor"] ? 
+        options["sleepFor"] : this.sleepFor;
+        
+      var chunkSize = options && options["chunkSize"] ? 
+        options["chunkSize"] : this.chunkSize;
+      
+      // Function to check if should stop
+      var shouldContinue = function shouldStop() {
+        var cont = !this.stopped && wh();
+        if (!cont && this.onStop && typeof this.onStop == "function") this.onStop(); 
+        return cont;
+      }
+      
+      // Execute a chunk of code
+      var chunk = function() {
+        var jskcurrent = 0;
+        while (shouldContinue.apply(that) && jskcurrent++ < chunkSize) {
+          fct();
+          inc();
+        } 
+        
+        if (shouldContinue.apply(that)) setTimeout(chunk, sleepFor);
+      }
+      
+      // Start the thread
+      chunk();
+      if (this.onStart && typeof this.onStart == "function") this.onStart();
+      
+      return this;
+    },
+    // Stop it after the next round
+    stop:function() {
+      this.stopped = true;
+    },
+    /// EVENTS
+    onStart : function() {
+      return false;
+    },
+    onStop : function() {
+      return false;
     }
   }
 
   // Creates a namespace if not exist
-  if (window.jskata == undefined) {
+  if (window.jskata === undefined) {
     window.jskata = {};
     window.jsk = window.jskata;
   }
