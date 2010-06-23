5c680189c766d6795335921d9bcc1acac795f3d2
Dan Simard <dsimard@gmail.com>
2010-06-23 10:53:51 -0400

    better nofreeze demo

---
+++ nofreeze.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  $(".stop").click(function() {$(this).fadeOut();});
+
+  /********** NON FREEZING FOR **********/
+  // A non-freezing for
+  var nonFreezingFor = function nonFreezingFor() {
+    var i = 0; // index
+    var loop = jsk.nofreeze.forloop(
+      function() { return i < Number.MAX_VALUE;  }, // the condition
+      function() { i++; }, // the incrementor
+      function fct() { // this is what will be executed
+         $("#nonFreezingCount").text(i.toString());
+      }
+    );
+    
+    // Stop
+    $("#nonFreezingStop").fadeIn().click(loop.stop);
+  }
+
+  $("#nonFreezingFor").click(nonFreezingFor);
+  
+  /********** INFINITE **********/
+  var infinite = function infinite() {
+    // Start the loop
+    var loop = jsk.nofreeze.infinite(function() {
+      $("#infiniteRandom").text(Math.round((Math.random()*1000)).toString())
+    });
+    
+    // Stop the loop
+    $("#stopInfinite").fadeIn().click(loop.stop);
+  }
+  
+  $("#infinite").click(infinite);
+  
+  /********** EACH **********/
+  var eachLoop = function eachLoop() {
+    _.nf.each(window, function(index, el) {
+      $("#windowEach").append("<span>"+index+", </span>");
+    });
+  }
+  
+  $("#each").click(eachLoop);
+});
+
