0a2ba93d61fbf12e06a9a543764e4a5a6b717ab4
Dan Simard <dsimard@gmail.com>
2010-10-21 10:29:41 -0400

    added node.js script for hook

---
+++ check_jslint.js
@@ -0,0 +1,27 @@
+    var sys = require("sys");
+    var fs = require("fs");
+    var cp = require('child_process');
+
+    var path = fs.realpathSync("./src/");
+
+    fs.readdir(fs.realpathSync("./src/"), function(err, files) {
+      for (var i = 0; i < files.length; i++) {
+        var filename = files[i];
+        var complete = path + filename;
+
+        // Run jslint on each file
+        var jslint = cp.exec("jslint " + complete, function(error, stdout, stderr) {
+          if (error) {
+            console.log(filename + " : " + error);
+          }
+        });    
+      }
+    });
+
+/*
+    jskata.nofreeze.js : /home/dan/php/jskata/src/jskata.undo.js
+
+    jskata.nofreeze.js : /home/dan/php/jskata/src/jskata.nofreeze.js
+
+    jskata.nofreeze.js : /home/dan/php/jskata/src/jskata.timezone.js
+*/
