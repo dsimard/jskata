9083c70a68a926f5740b83ae546771662eae09fa
Dan Simard <dsimard@gmail.com>
2010-10-22 10:15:43 -0400

    still unsure how to compress js

--- check_jslint.js
+++ check_jslint.js
@@ -4,17 +4,21 @@ var cp = require('child_process');
 
 var path = fs.realpathSync("./src/");
 
+var inError = false;
+
 fs.readdir(fs.realpathSync("./src/"), function(err, files) {
   for (var i = 0; i < files.length; i++) {
     var filename = files[i];
     var complete = path + filename;
-
-    // Run jslint on each file
-    var jslint = cp.exec("jslint " + complete, function(error, stdout, stderr) {
-      if (error) {
-        console.log(filename + " : " + error);
-      }
-    });    
+    
+    (function(file) {
+      // Run jslint on each file
+      var jslint = cp.exec("jslint " + file, function(error, stdout, stderr) {
+        if (error) {
+          inError = true;
+          console.log(file + " : " + error);
+        }
+      }); 
+    })(complete);   
   }
 });
-
